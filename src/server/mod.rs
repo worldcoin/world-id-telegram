b4986616ca2fe63a368497a7c63f5ad7d05b5ec7
Miguel Piedrafita <github@miguel.build>
2023-12-11 15:27:49 +0100

    switch to IDKit v1

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -64,7 +64,7 @@ async fn verify_page(
                 <title>Verify with World ID</title>
             </head>
             <body>
-                <script src=\"https://unpkg.com/@worldcoin/idkit-standalone@1.0.0-alpha.4/build/index.global.js\"></script>
+                <script src=\"https://unpkg.com/@worldcoin/idkit-standalone/build/index.global.js\"></script>
 
                 <script>
                     IDKit.init({{
